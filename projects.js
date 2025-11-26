be9e55827fd65710e445f38cb4f89bc4b94396db
owwwy <owanley@yandex.ru>
2025-11-26 15:10:40 +0300

    Update and rename project.js to projects.js

--- projects.js
+++ projects.js
@@ -3,7 +3,7 @@ async function loadProjects() {
   if (!container) return;
 
   try {
-    const response = await fetch('project.json');
+    const response = await fetch('projects.json');
     if (!response.ok) {
       console.error('Не удалось загрузить projects.json', response.status);
       return;
